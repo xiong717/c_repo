2a352b5eb0d02d528e6fdd15135aaf6701e11d88
xiong717 <1304158615@qq.com>
2020-11-20 23:24:01 +0800

    喝汽水问题

---
+++ 2020_11_20day10 结构体/test0.c
@@ -0,0 +1,23 @@
+//喝汽水问题 20yuan=20->10->5->2(1)->1(1)->1
+//喝汽水，1瓶汽水1元，2个空瓶可以换一瓶汽水，给20元，可以多少汽水（编程实现）
+#include<stdio.h>
+#include<windows.h>
+ 
+int main()
+{
+	int total = 0;
+	int money = 1;
+	int empty = 0;
+	scanf_s("%d", &money);
+	total = money;
+	empty = money;
+	while (empty >1)
+	{
+		money = empty / 2;
+		empty = empty / 2 + empty % 2;
+		total += money;
+	}
+	printf("可以换%d瓶\n", total);
+	system("pause");
+	return 0;
+}
